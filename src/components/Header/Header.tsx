f1bd46e1ceb80bad6ff728e2c6b435f62e1aea5d
Oleg <oleg_g87@outlook.com>
2023-06-05 18:29:53 +0300

    add theme switcher

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,13 @@
+import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher'
 import style from './Header.module.scss'
 
-// import iconMoon from '../../assets/images/moon.svg'
-
 export default function Header(): JSX.Element {
-  return (
-    <header className={style.header}>
-      <div className={style.container}>
-        <h1 className={style.heading}>Where in the world?</h1>
-        {/* <button className={style.themeSwitcher}>
-          <img src={iconMoon} alt="moon" />
-          <span>Dark Mode</span>
-        </button> */}
-      </div>
-    </header>
-  )
+    return (
+        <header className={style.header}>
+            <div className={style.container}>
+                <h1 className={style.heading}>Where in the world?</h1>
+                <ThemeSwitcher />
+            </div>
+        </header>
+    )
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,30 +1,44 @@
 import { useMemo, useState, useDeferredValue } from 'react'
 import { Card, LoadingIndicator } from '../../components'
 import { useShortCountryInfo, TShortCountryInfo } from '../../hooks'
-import iconSearch from '../../assets/images/magnifying-glass.svg'
 import style from './HomePage.module.scss'
 
 const DEFAULT_FILTER_VALUE = 'All'
 
 export default function HomePage(): JSX.Element {
     const [searchQuery, setSearchQuery] = useState('')
-    const [regionFilterValue, setRegionFilterValue] = useState(DEFAULT_FILTER_VALUE)
+    const [regionFilterVal, setRegionFilterVal] = useState(DEFAULT_FILTER_VALUE)
     const [countryInfoList, isLoading, isError, error] = useShortCountryInfo()
     const deferredQuery = useDeferredValue(searchQuery)
 
-    const fileteredCountryInfoList = useMemo(() => {
-        // filters
+    // const fileteredCountryInfoList = useMemo(() => {
+    //     // filters
+    //     let filtered = countryInfoList
+    //     if (regionFilterValue !== DEFAULT_FILTER_VALUE)
+    //         filtered = filtered.filter((countryInfo: TShortCountryInfo) =>
+    //             countryInfo.region === regionFilterValue)
+    //     // search
+    //     if (!searchQuery)
+    //         return filtered
+    //     return filtered.filter((countryInfo: TShortCountryInfo) =>
+    //         countryInfo.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+    //     )
+    // }, [deferredQuery, isLoading, regionFilterValue])
+
+    let fileteredList = useMemo(() => {
         let filtered = countryInfoList
-        if (regionFilterValue !== DEFAULT_FILTER_VALUE)
-            filtered = filtered.filter((countryInfo: TShortCountryInfo) =>
-                countryInfo.region === regionFilterValue)
-        // search
+        if (regionFilterVal !== DEFAULT_FILTER_VALUE)
+            filtered = filtered.filter((ci: TShortCountryInfo) =>
+                ci.region === regionFilterVal)
+        return filtered
+    }, [isLoading, regionFilterVal])
+
+    fileteredList = useMemo(() => {
         if (!searchQuery)
-            return filtered
-        return filtered.filter((countryInfo: TShortCountryInfo) =>
-            countryInfo.name.toLowerCase().startsWith(searchQuery.toLowerCase())
-        )
-    }, [deferredQuery, isLoading, regionFilterValue])
+            return fileteredList
+        return fileteredList.filter((ci: TShortCountryInfo) =>
+            ci.name.toLowerCase().startsWith(searchQuery.toLowerCase()))
+    }, [deferredQuery, isLoading, regionFilterVal])
 
     if (isLoading)
         return <LoadingIndicator />
@@ -41,9 +55,11 @@ export default function HomePage(): JSX.Element {
                         placeholder="Search for a country..."
                         onChange={(e) => setSearchQuery(e.target.value)}
                     />
-                    <img className={style.searchInputIcon} src={iconSearch} alt="" />
+                    <svg className={style.searchInputIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                    </svg>
                 </div>
-                <select className={style.selectRegionFilter} defaultValue="" onChange={(e) => setRegionFilterValue(e.target.value)}>
+                <select className={style.selectRegionFilter} defaultValue="" onChange={(e) => setRegionFilterVal(e.target.value)}>
                     <option value="" disabled={true}>Filter by Region</option>
                     <option value={DEFAULT_FILTER_VALUE}>All</option>
                     <option value="Africa">Africa</option>
@@ -55,7 +71,7 @@ export default function HomePage(): JSX.Element {
             </div>
             <div className={style.cardList}>
                 {
-                    fileteredCountryInfoList.map((countryInfo: TShortCountryInfo) => (
+                    fileteredList.map((countryInfo: TShortCountryInfo) => (
                         <Card
                             key={countryInfo.name}
                             flagImage={countryInfo.flag.img}
